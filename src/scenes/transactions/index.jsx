13504720be9eea3163f92c9f3d9d64a84b20b9a0
RightShiner <brilight.shiner@gmail.com>
2023-02-07 18:53:55 -0800

    fix: fix container name invoice to transactions

--- src/scenes/transactions/index.jsx
+++ src/scenes/transactions/index.jsx
@@ -4,7 +4,7 @@ import { tokens } from "../../theme";
 import { mockDataInvoices } from "../../data/mockData";
 import Header from "../../components/Header";
 
-const Invoices = () => {
+const Transactions = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const columns = [
@@ -80,4 +80,4 @@ const Invoices = () => {
   );
 };
 
-export default Invoices;
+export default Transactions;
