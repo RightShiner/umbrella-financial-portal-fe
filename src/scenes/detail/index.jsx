acbf424397cf5713e80f50f959771a22b75cfc42
RightShiner <brilight.shiner@gmail.com>
2023-02-19 23:53:48 +0100

    feat(sales-db-bind) : bind sales page and detail page with db

--- src/scenes/detail/index.jsx
+++ src/scenes/detail/index.jsx
@@ -29,11 +29,17 @@ const Detail = () => {
   const colors = tokens(theme.palette.mode);
   const [sales, setSales] = useState([]);
   const [sellingUser, setSellingUser] = useState([]);
+  const [purchasingUser, setPurchasingUser] = useState([]);
+  const [product, setProduct] = useState([]);
+  const [commissionPlan, setCommissionPlan] = useState([]);
   const { user, sessionToken, saletemp } = useContext(UserContext);
 
   useEffect(() => {
     setSales([saletemp]);
     setSellingUser([saletemp.sellingUser]);
+    setPurchasingUser([saletemp.purchasingUser]);
+    setProduct([saletemp.product]);
+    setCommissionPlan([saletemp.commissionPlan]);
     console.log(sellingUser);
     // setSales([
     //   {
@@ -222,7 +228,7 @@ const Detail = () => {
       <Accordion defaultExpanded>
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <Typography color={colors.greenAccent[500]} variant="h5">
-            Commission Plan Infomation
+            PurchasingUser Infomation
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
@@ -237,20 +243,32 @@ const Detail = () => {
                 <TableHead>
                   <TableRow>
                     <TableCell>id</TableCell>
-                    <TableCell align="right">sellingUserId</TableCell>
-                    <TableCell align="right">purchasingUserId</TableCell>
-                    <TableCell align="right">productId</TableCell>
-                    <TableCell align="right">purchasePrice</TableCell>
-                    <TableCell align="right">trialStartDate</TableCell>
-                    <TableCell align="right">billingStartDate</TableCell>
+                    <TableCell align="right">uigUserId</TableCell>
+                    <TableCell align="right">type</TableCell>
+                    <TableCell align="right">role</TableCell>
+                    <TableCell align="right">hashPassword</TableCell>
+                    <TableCell align="right">level1ReferredByUserId</TableCell>
+                    <TableCell align="right">level2ReferredByUserId</TableCell>
+                    <TableCell align="right">level3ReferredByUserId</TableCell>
+                    <TableCell align="right">affiliateStatus</TableCell>
+                    <TableCell align="right">affiliateUrl</TableCell>
+                    <TableCell align="right">name</TableCell>
+                    <TableCell align="right">email</TableCell>
+                    <TableCell align="right">phoneNumber</TableCell>
+                    <TableCell align="right">username</TableCell>
+                    <TableCell align="right">dateOfBirth</TableCell>
+                    <TableCell align="right">primaryGhlLocationId</TableCell>
+                    <TableCell align="right">ghlUserId</TableCell>
+                    <TableCell align="right">
+                      ghlSuperCorporateContactId
+                    </TableCell>
+                    <TableCell align="right">salesAccountId</TableCell>
                     <TableCell align="right">dateCreated</TableCell>
                     <TableCell align="right">dateUpdated</TableCell>
-                    <TableCell align="right">dateCancelled</TableCell>
-                    <TableCell align="right">commissionPlanId</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {sales.map((row) => (
+                  {purchasingUser.map((row) => (
                     <TableRow
                       key={row.id}
                       sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -258,10 +276,175 @@ const Detail = () => {
                       <TableCell component="th" scope="row">
                         {row.id}
                       </TableCell>
-                      <TableCell align="right">{row.purchasePrice}</TableCell>
-                      <TableCell align="right">{row.sellingUserName}</TableCell>
+                      <TableCell align="right">{row.uigUserId}</TableCell>
+                      <TableCell align="right">{row.type}</TableCell>
+                      <TableCell align="right">{row.role}</TableCell>
+                      <TableCell align="right">{row.hashPassword}</TableCell>
                       <TableCell align="right">
-                        {row.purchasingUserName}
+                        {row.level1ReferredByUserId}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.level2ReferredByUserId}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.level3ReferredByUserId}
+                      </TableCell>
+                      <TableCell align="right">{row.affiliateStatus}</TableCell>
+                      <TableCell align="right">{row.affiliateUrl}</TableCell>
+                      <TableCell align="right">{row.name}</TableCell>
+                      <TableCell align="right">{row.email}</TableCell>
+                      <TableCell align="right">{row.phoneNumber}</TableCell>
+                      <TableCell align="right">{row.username}</TableCell>
+                      <TableCell align="right">{row.dateOfBirth}</TableCell>
+                      <TableCell align="right">
+                        {row.primaryGhlLocationId}
+                      </TableCell>
+                      <TableCell align="right">{row.ghlUserId}</TableCell>
+                      <TableCell align="right">
+                        {row.ghlSuperCorporateContactId}
+                      </TableCell>
+                      <TableCell align="right">{row.salesAccountId}</TableCell>
+                      <TableCell align="right">{row.dateCreated}</TableCell>
+                      <TableCell align="right">{row.dateUpdated}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Product Infomation
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <TableContainer component={Paper}>
+              <Table
+                sx={{
+                  minWidth: 650,
+                }}
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell>id</TableCell>
+                    <TableCell align="right">name</TableCell>
+                    <TableCell align="right">isRecurring</TableCell>
+                    <TableCell align="right">billingPeriodLength</TableCell>
+                    <TableCell align="right">billingPeriodUnit</TableCell>
+                    <TableCell align="right">trialPeriodLength</TableCell>
+                    <TableCell align="right">trialPeriodUnit</TableCell>
+                    <TableCell align="right">retailPrice</TableCell>
+                    <TableCell align="right">costAmount</TableCell>
+                    <TableCell align="right">dateCreated</TableCell>
+                    <TableCell align="right">dateUpdated</TableCell>
+                    <TableCell align="right">dateRetired</TableCell>
+                    <TableCell align="right">defaultCommissionPlanId</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {product.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.id}
+                      </TableCell>
+                      <TableCell align="right">{row.name}</TableCell>
+                      <TableCell align="right">{row.isRecurring}</TableCell>
+                      <TableCell align="right">
+                        {row.billingPeriodLength}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.billingPeriodUnit}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.trialPeriodLength}
+                      </TableCell>
+                      <TableCell align="right">{row.trialPeriodUnit}</TableCell>
+                      <TableCell align="right">{row.retailPrice}</TableCell>
+                      <TableCell align="right">{row.costAmount}</TableCell>
+                      <TableCell align="right">{row.dateCreated}</TableCell>
+                      <TableCell align="right">{row.dateUpdated}</TableCell>
+                      <TableCell align="right">{row.dateRetired}</TableCell>
+                      <TableCell align="right">
+                        {row.defaultCommissionPlanId}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            CommissionPlan Infomation
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <TableContainer component={Paper}>
+              <Table
+                sx={{
+                  minWidth: 650,
+                }}
+                aria-label="simple table"
+              >
+                <TableHead>
+                  <TableRow>
+                    <TableCell>id</TableCell>
+                    <TableCell align="right">name</TableCell>
+                    <TableCell align="right">
+                      commissionableValueShare
+                    </TableCell>
+                    <TableCell align="right">personalSaleShare</TableCell>
+                    <TableCell align="right">
+                      corporatePartnerProfitShare
+                    </TableCell>
+                    <TableCell align="right">level1ReferralShare</TableCell>
+                    <TableCell align="right">level2ReferralShare</TableCell>
+                    <TableCell align="right">level3ReferralShare</TableCell>
+                    <TableCell align="right">dateCreated</TableCell>
+                    <TableCell align="right">dateUpdated</TableCell>
+                    <TableCell align="right">dateRetired</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {commissionPlan.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.id}
+                      </TableCell>
+                      <TableCell align="right">{row.name}</TableCell>
+                      <TableCell align="right">{row.isRecurring}</TableCell>
+                      <TableCell align="right">
+                        {row.billingPeriodLength}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.billingPeriodUnit}
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.trialPeriodLength}
+                      </TableCell>
+                      <TableCell align="right">{row.trialPeriodUnit}</TableCell>
+                      <TableCell align="right">{row.retailPrice}</TableCell>
+                      <TableCell align="right">{row.costAmount}</TableCell>
+                      <TableCell align="right">{row.dateCreated}</TableCell>
+                      <TableCell align="right">{row.dateUpdated}</TableCell>
+                      <TableCell align="right">{row.dateRetired}</TableCell>
+                      <TableCell align="right">
+                        {row.defaultCommissionPlanId}
                       </TableCell>
                     </TableRow>
                   ))}
--- src/scenes/sales/index.jsx
+++ src/scenes/sales/index.jsx
@@ -18,38 +18,38 @@ const Sales = ({ setSaletemp }) => {
   const [sales, setSales] = useState([]);
   const [selectedRow, setSelectedRow] = useState(null);
 
-  //   useEffect(() => {
-  //     axios({
-  //       method: "get",
-  //       url: `https://umbrella.rest.ghlmanager.com/sales`,
-  //       headers: {
-  //         Authorization: `Bearer ${sessionToken}`,
-  //       },
-  //     })
-  //       .then(function (response) {
-  //         const salesData = response.data.sales;
-  //         console.log(response);
-  //         for (const [index, sale] of salesData.entries()) {
-  //           sale.sellingUserName = sale.sellingUser.name;
-  //           sale.purchasingUserName = sale.purchasingUser.name;
-  //           sale.purchasePrice = Number(sale.purchasePrice);
-  //         }
-  //         setSales(salesData);
-  //       })
-  //       .catch(function (err) {
-  //         console.log(err);
-  //       });
-  //   }, []);
   useEffect(() => {
-    setSales([
-      {
-        id: "1",
-        purchasePrice: "5000",
-        sellingUserName: "ddd",
-        purchasingUserName: "ddd",
+    axios({
+      method: "get",
+      url: `https://umbrella.rest.ghlmanager.com/sales`,
+      headers: {
+        Authorization: `Bearer ${sessionToken}`,
       },
-    ]);
+    })
+      .then(function (response) {
+        const salesData = response.data.sales;
+        console.log(response);
+        for (const [index, sale] of salesData.entries()) {
+          sale.sellingUserName = sale.sellingUser.name;
+          sale.purchasingUserName = sale.purchasingUser.name;
+          sale.purchasePrice = Number(sale.purchasePrice);
+        }
+        setSales(salesData);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }, []);
+  //   useEffect(() => {
+  //     setSales([
+  //       {
+  //         id: "1",
+  //         purchasePrice: "5000",
+  //         sellingUserName: "ddd",
+  //         purchasingUserName: "ddd",
+  //       },
+  //     ]);
+  //   }, []);
 
   const handleRowClick = (params) => {
     const id = params.row.id;
@@ -61,116 +61,116 @@ const Sales = ({ setSaletemp }) => {
     // console.log(selectedRow);
     // navigate(`/sales/${id}`);
 
-    // const pushData = async () => {
-    //   axios({
-    //     method: "get",
-    //     url: `https://umbrella.rest.ghlmanager.com/sales/${id}`,
-    //     headers: {
-    //       Authorization: `Bearer ${sessionToken}`,
-    //     },
-    //   })
-    //     .then(function (response) {
-    //       //   console.log(response.data.sale);
-    //       setSaletemp(response.data.sale);
-    //       console.log(saletemp);
-    //       navigate("/detail");
-    //     })
-    //     .catch(function (err) {
-    //       console.log(err);
-    //     });
-    // };
-    // pushData();
-    setSaletemp({
-      id: "027afb95-2d5a-4189-b73b-77a159b8ef71",
-      sellingUserId: "3f638d14-2efa-40ab-ae4e-3351e70ff704",
-      purchasingUserId: "80829dac-aa5f-4b71-82e0-3f94b0113728",
-      productId: "5092bc95-cba6-4cb4-8593-aa1fe96825e8",
-      purchasePrice: "700",
-      trialStartDate: null,
-      billingStartDate: null,
-      dateCreated: "2023-02-18T04:13:42.409Z",
-      dateUpdated: "2023-02-18T04:13:42.409Z",
-      dateCancelled: null,
-      commissionPlanId: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
-      sellingUser: {
-        id: "3f638d14-2efa-40ab-ae4e-3351e70ff704",
-        uigUserId: null,
-        type: "AFFILIATE",
-        role: "USER",
-        hashPassword:
-          "$2b$14$qZ9VBo.J48SiPq19fhLwke4Cx/.OUjLqiENKlErskxsA5Rw/.4y/W",
-        level1ReferredByUserId: null,
-        level2ReferredByUserId: null,
-        level3ReferredByUserId: null,
-        affiliateStatus: null,
-        affiliateUrl: null,
-        name: "DENNISE RAMBERANSING",
-        email: null,
-        phoneNumber: null,
-        username: null,
-        dateOfBirth: null,
-        primaryGhlLocationId: null,
-        ghlUserId: null,
-        ghlSuperCorporateContactId: null,
-        salesAccountId: null,
-        dateCreated: "2023-02-18T04:13:42.409Z",
-        dateUpdated: "2023-02-18T04:13:42.409Z",
-      },
-      purchasingUser: {
-        id: "80829dac-aa5f-4b71-82e0-3f94b0113728",
-        uigUserId: null,
-        type: "CUSTOMER",
-        role: "USER",
-        hashPassword:
-          "$2b$14$qZ9VBo.J48SiPq19fhLwke4Cx/.OUjLqiENKlErskxsA5Rw/.4y/W",
-        level1ReferredByUserId: null,
-        level2ReferredByUserId: null,
-        level3ReferredByUserId: null,
-        affiliateStatus: null,
-        affiliateUrl: null,
-        name: "DENNIS RAMBERANSINGH",
-        email: null,
-        phoneNumber: null,
-        username: null,
-        dateOfBirth: null,
-        primaryGhlLocationId: null,
-        ghlUserId: null,
-        ghlSuperCorporateContactId: null,
-        salesAccountId: null,
-        dateCreated: "2023-02-18T04:13:42.409Z",
-        dateUpdated: "2023-02-18T04:13:42.409Z",
-      },
-      product: {
-        id: "5092bc95-cba6-4cb4-8593-aa1fe96825e8",
-        name: "Taxes level 1",
-        isRecurring: false,
-        billingPeriodLength: null,
-        billingPeriodUnit: null,
-        trialPeriodLength: null,
-        trialPeriodUnit: null,
-        retailPrice: "500",
-        costAmount: "150",
-        dateCreated: "2023-02-18T04:13:07.050Z",
-        dateUpdated: "2023-02-18T04:13:07.050Z",
-        dateRetired: null,
-        defaultCommissionPlanId: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
-      },
-      commissionPlan: {
-        id: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
-        name: "Fixed Fee Standard Commission Plan",
-        commissionableValueShare: "1",
-        personalSaleShare: "0.3",
-        corporatePartnerProfitShare: "0.4",
-        level1ReferralShare: "0.1",
-        level2ReferralShare: "0.05",
-        level3ReferralShare: "0.03",
-        dateCreated: "2023-02-18T04:13:06.645Z",
-        dateUpdated: "2023-02-18T04:13:06.645Z",
-        dateRetired: null,
-      },
-    });
+    const pushData = async () => {
+      axios({
+        method: "get",
+        url: `https://umbrella.rest.ghlmanager.com/sales/${id}`,
+        headers: {
+          Authorization: `Bearer ${sessionToken}`,
+        },
+      })
+        .then(function (response) {
+          console.log(response.data.sale);
+          setSaletemp(response.data.sale);
+          console.log(saletemp);
+          navigate("/detail");
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    };
+    pushData();
+    // setSaletemp({
+    //   id: "027afb95-2d5a-4189-b73b-77a159b8ef71",
+    //   sellingUserId: "3f638d14-2efa-40ab-ae4e-3351e70ff704",
+    //   purchasingUserId: "80829dac-aa5f-4b71-82e0-3f94b0113728",
+    //   productId: "5092bc95-cba6-4cb4-8593-aa1fe96825e8",
+    //   purchasePrice: "700",
+    //   trialStartDate: null,
+    //   billingStartDate: null,
+    //   dateCreated: "2023-02-18T04:13:42.409Z",
+    //   dateUpdated: "2023-02-18T04:13:42.409Z",
+    //   dateCancelled: null,
+    //   commissionPlanId: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
+    //   sellingUser: {
+    //     id: "3f638d14-2efa-40ab-ae4e-3351e70ff704",
+    //     uigUserId: null,
+    //     type: "AFFILIATE",
+    //     role: "USER",
+    //     hashPassword:
+    //       "$2b$14$qZ9VBo.J48SiPq19fhLwke4Cx/.OUjLqiENKlErskxsA5Rw/.4y/W",
+    //     level1ReferredByUserId: null,
+    //     level2ReferredByUserId: null,
+    //     level3ReferredByUserId: null,
+    //     affiliateStatus: null,
+    //     affiliateUrl: null,
+    //     name: "DENNISE RAMBERANSING",
+    //     email: null,
+    //     phoneNumber: null,
+    //     username: null,
+    //     dateOfBirth: null,
+    //     primaryGhlLocationId: null,
+    //     ghlUserId: null,
+    //     ghlSuperCorporateContactId: null,
+    //     salesAccountId: null,
+    //     dateCreated: "2023-02-18T04:13:42.409Z",
+    //     dateUpdated: "2023-02-18T04:13:42.409Z",
+    //   },
+    //   purchasingUser: {
+    //     id: "80829dac-aa5f-4b71-82e0-3f94b0113728",
+    //     uigUserId: null,
+    //     type: "CUSTOMER",
+    //     role: "USER",
+    //     hashPassword:
+    //       "$2b$14$qZ9VBo.J48SiPq19fhLwke4Cx/.OUjLqiENKlErskxsA5Rw/.4y/W",
+    //     level1ReferredByUserId: null,
+    //     level2ReferredByUserId: null,
+    //     level3ReferredByUserId: null,
+    //     affiliateStatus: null,
+    //     affiliateUrl: null,
+    //     name: "DENNIS RAMBERANSINGH",
+    //     email: null,
+    //     phoneNumber: null,
+    //     username: null,
+    //     dateOfBirth: null,
+    //     primaryGhlLocationId: null,
+    //     ghlUserId: null,
+    //     ghlSuperCorporateContactId: null,
+    //     salesAccountId: null,
+    //     dateCreated: "2023-02-18T04:13:42.409Z",
+    //     dateUpdated: "2023-02-18T04:13:42.409Z",
+    //   },
+    //   product: {
+    //     id: "5092bc95-cba6-4cb4-8593-aa1fe96825e8",
+    //     name: "Taxes level 1",
+    //     isRecurring: false,
+    //     billingPeriodLength: null,
+    //     billingPeriodUnit: null,
+    //     trialPeriodLength: null,
+    //     trialPeriodUnit: null,
+    //     retailPrice: "500",
+    //     costAmount: "150",
+    //     dateCreated: "2023-02-18T04:13:07.050Z",
+    //     dateUpdated: "2023-02-18T04:13:07.050Z",
+    //     dateRetired: null,
+    //     defaultCommissionPlanId: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
+    //   },
+    //   commissionPlan: {
+    //     id: "e3e334a9-c483-4ce9-b840-b188046fb9fb",
+    //     name: "Fixed Fee Standard Commission Plan",
+    //     commissionableValueShare: "1",
+    //     personalSaleShare: "0.3",
+    //     corporatePartnerProfitShare: "0.4",
+    //     level1ReferralShare: "0.1",
+    //     level2ReferralShare: "0.05",
+    //     level3ReferralShare: "0.03",
+    //     dateCreated: "2023-02-18T04:13:06.645Z",
+    //     dateUpdated: "2023-02-18T04:13:06.645Z",
+    //     dateRetired: null,
+    //   },
+    // });
     // console.log(saletemp);
-    navigate("/detail");
+    // navigate("/detail");
   };
 
   //   useEffect(() => {
